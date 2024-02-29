67fef9b852739fa882a61ef806e5f606e10e7209
Przemysław Stachurski <przemekstachurski737@gmail.com>
2024-02-29 21:05:07 +0100

    Added apping java objects to relational tables and saving objects

--- cruddemo/src/main/java/com/studying/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/studying/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.studying.cruddemo;
 
+import com.studying.cruddemo.dao.StudentDAO;
+import com.studying.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,35 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 
 		return runner-> {
-			System.out.println("Hello World");
+			//createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO){
+		System.out.println("creating 3 students");
+		Student temopStudent = new Student("Jan","Kowalski","mail@email.com");
+		Student temopStudent2 = new Student("Jano","Nowak","mail@email.com");
+		Student temopStudent3 = new Student("Janek","Wisniewski","mail@email.com");
+
+		System.out.println("saving students");
+		studentDAO.save(temopStudent);
+		studentDAO.save(temopStudent2);
+		studentDAO.save(temopStudent3);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating student object");
+		Student temopStudent = new Student("Jan","Kowalski","number3@email.com");
+
+		System.out.println("saving student to db");
+		studentDAO.save(temopStudent);
+
+		System.out.println("displaying student`s generated id:");
+		System.out.println(temopStudent.getId());
+
 	}
 }
---
+++ cruddemo/src/main/java/com/studying/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.studying.cruddemo.dao;
+
+import com.studying.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
