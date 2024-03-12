578ceb66fbf959006b981408654ac529df0c39ef
Przemysław Stachurski <przemekstachurski737@gmail.com>
2024-03-12 19:38:45 +0100

    Added new rest project using spring data jpa

---
+++ employeecrud spring data jpa/src/main/java/com/example/employeecrud/rest/EmployeeRESTController.java
@@ -0,0 +1,68 @@
+package com.example.employeecrud.rest;
+
+
+import com.example.employeecrud.entity.Employee;
+import com.example.employeecrud.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRESTController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeRESTController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee findById(@PathVariable int id) {
+
+        Employee employee = employeeService.findById(id);
+
+        if (employee == null) {
+            throw new RuntimeException("employee not found");
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee){
+
+        employee.setId(0);
+
+        Employee addedEmployee = employeeService.save(employee);
+
+        return addedEmployee;
+    }
+
+    @PutMapping("employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+
+        Employee updatedEmployee = employeeService.save(employee);
+
+        return updatedEmployee;
+    }
+
+    @DeleteMapping("employees/{id}")
+    public String deleteEmployee(@PathVariable int id){
+
+        Employee employee = employeeService.findById(id);
+
+        if (employee==null){
+            throw new RuntimeException("employee not found in the database");
+        }
+
+        employeeService.deleteById(id);
+
+        return "Deleted employee with id = " + id;
+    }
+}
---
+++ employeecrud spring data jpa/src/main/java/com/example/employeecrud/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.example.employeecrud.service;
+
+import com.example.employeecrud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int id);
+
+    Employee save(Employee employee);
+
+    void deleteById(int id);
+}
